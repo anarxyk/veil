9b7faab89a7586d70f6129ecc0025cd15b9a736c
anarxyk <himsohimothy@gmail.com>
2026-09-02 15:47:28 -0700

    Update input.zig

--- zig/input.zig
+++ zig/input.zig
@@ -9,9 +9,16 @@ const KeyEvent = extern struct {
 
 extern fn keyboard_read(event: *KeyEvent) callconv(.c) c_int;
 extern fn console_put(value: byte) callconv(.c) void;
+extern fn serial_get() callconv(.c) c_int;
 
 export fn input_poll() callconv(.c) void {
     var event: KeyEvent = undefined;
+    var value: c_int = serial_get();
+
+    while (value >= 0) {
+        console_put(@as(byte, @intCast(value)));
+        value = serial_get();
+    }
     while (keyboard_read(&event) != 0) {
         if (event.state != 0) continue;
         console_put(event.ascii);
